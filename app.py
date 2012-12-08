7b72c5dac06dd9dc0d59c2470206e9403bb3a22f
jensa <jensa@jensa-VirtualBox.(none)>
2012-12-08 19:51:29 +0100

    getting a simple jquery ajax-behaviour working

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+@app.route('/_game_search')
+def game_search():
+    print "call made"
+    data = 'push_this'
+    return jsonify(gamelist=data)
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('tmp.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
