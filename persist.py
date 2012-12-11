3f90a6b084618911f9a509d595c791fe8e75365d
jensa <jensa@jensa-VirtualBox.(none)>
2012-12-12 00:13:16 +0100

    test

--- persist.py
+++ persist.py
@@ -11,8 +11,9 @@ class Database():
 
             # Get a connection
             conn = pymongo.Connection(os.environ.get('MONGOHQ_URL'))
+            print
             # Get the database
-            db = conn[urlparse(os.environ.get('MONGOHQ_URL')).path[2:]]
+            db = conn[urlparse(os.environ.get('MONGOHQ_URL')).path[3:]]
         except:
             # Not on an app with the MongoHQ add-on, do some localhost action
             conn = pymongo.Connection('localhost', 27017)
