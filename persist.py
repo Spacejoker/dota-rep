e71876134457acf8fd3b273b68e504612707b1f2
jensa <jensa@jensa-VirtualBox.(none)>
2012-12-11 23:03:38 +0100

    minor db tweaks

--- persist.py
+++ persist.py
@@ -7,12 +7,12 @@ class Database():
     def __init__(self):
         print 'initializing db connection'
         try:
-            if os.environ['MONGOHQ_URL']:
+            print "HERE: " + os.environ.get('MONGOHQ_URL'):
 
             # Get a connection
-                conn = pymongo.Connection(os.environ['MONGOHQ_URL'])
+                conn = pymongo.Connection(os.environ.get('MONGOHQ_URL'))
             # Get the database
-                db = conn[urlparse(os.environ['MONGOHQ_URL']).path[1:]]
+                db = conn[urlparse(os.environ.get('MONGOHQ_URL')).path[1:]]
         except:
             # Not on an app with the MongoHQ add-on, do some localhost action
             conn = pymongo.Connection('localhost', 27017)
