48c4fdb190c7f89b71151504dd04bddedca61216
jensa <jensa@jensa-VirtualBox.(none)>
2012-12-12 08:42:16 +0100

    test

--- persist.py
+++ persist.py
@@ -1,20 +1,23 @@
 from model import *
 import pymongo 
 import os
+import traceback
 
 class Database():
 
     def __init__(self):
         print 'initializing db connection'
         try:
-            print "HERE: " + os.environ.get('MONGOHQ_URL')
+            #print "HERE: " + os.environ.get('MONGOHQ_URL')
 
             # Get a connection
             conn = pymongo.Connection(os.environ.get('MONGOHQ_URL'))
             print
             # Get the database
-            db = conn[urlparse(os.environ.get('MONGOHQ_URL')).path[3:]]
+            db = conn[urlparse(os.environ.get('MONGOHQ_URL')).path[1:]]
         except:
+            tb = traceback.format_exc()
+            print tb
             # Not on an app with the MongoHQ add-on, do some localhost action
             conn = pymongo.Connection('localhost', 27017)
             db = conn['dota']
